47ddceb685cdc91a8a049d211dc1e3583e27da07
Irine <irinesmith567@gmail.com>
2021-01-06 17:57:06 +0200

    lesson 15 part 2 - +if constructions +check of input

--- taskMovies/script.js
+++ taskMovies/script.js
@@ -1,4 +1,5 @@
-let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', 3);
+'use strict'
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', 3);
 
 let personalMoviesDB = {
     count: numberOfFilms,
@@ -11,11 +12,27 @@ let i = 0
 do {
     let movie = prompt('Один из последних просотренных фильмов?', 'Разум и чувства');
     let mark = prompt('На сколько оцените его?', 7.5);
-    // for (let key in personalMoviesDB.movies) {
-    //     
-    // }
-    personalMoviesDB.movies[movie] = String(mark)
-    i++;
+    if (mark === null || mark === "" || movie === null || movie === "") {
+        alert("Пожалуйста, введите название и оценку!"); 
+        continue;
+    }
+    else if (movie.length > 50) {
+        alert("Название должно содержать меньше 50 символов.");
+        continue;
+    }
+    else {
+        personalMoviesDB.movies[movie] = String(mark)
+        i++;
+    }
 } while( i < 2 )
+
+if (personalMoviesDB.count < 10)
+    alert("Просмотрено довольно мало фильмов")
+else if (personalMoviesDB.count >= 10 || personalMoviesDB.count < 30)
+    alert("Вы классический зритель")
+else if (personalMoviesDB.count >= 30)
+    alert("Вы киноман")
+else 
+    alert("Произошла ошибка")
 
 console.log(personalMoviesDB);
